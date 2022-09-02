ed08e0f685f954efb3afc6b71b364e571a04c3a4
gero <geronicola1696@gmail.com>
2022-09-01 22:32:37 -0300

    inicio modal

---
+++ app.js
@@ -0,0 +1,4 @@
+const modal = document.getElementById("btn-ingresar")
+modal.addEventListener("click", function() {
+    window.open("http://www.google.com/", "MsgWindow", "width=400,height=600")
+})
