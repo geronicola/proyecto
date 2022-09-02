3b060a19b78c9c60813890d9784fca7a040074dc
Gaston <gastonhecht@gmail.com>
2022-09-02 20:39:59 -0300

    continuación modal

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-const modal = document.getElementById("btn-ingresar")
-modal.addEventListener("click", function() {
-    window.open("http://www.google.com/", "MsgWindow", "width=400,height=600")
+const modal = docmet.getElementById("modal")
+const openButton = document.getElementById("btn-ingresar")
+const closeButtonArrow = document.getElementById("btn-close-arrow")
+const closeButtonCross = document.getElementById("btn-close-cross")
+
+openButton.addEventListener("click", function(){
+    modal.style.display = "block";
 })
+
+closeButtonArrow.addEventListener("click", function(){
+    modal.style.display = "none";
+})
+
+closeButtonCross.addEventListener("click", function(){
+    modal.style.display = "none";
+})
+
+
+
+
+
