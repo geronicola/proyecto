3cd67b6588d48ced12127a9b1035a1abcb82dc1f
gero <geronicola1696@gmail.com>
2022-09-02 22:01:43 -0300

    final modal

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-const modal = docmet.getElementById("modal")
+const modal = document.getElementById("modal")
 const openButton = document.getElementById("btn-ingresar")
 const closeButtonArrow = document.getElementById("btn-close-arrow")
 const closeButtonCross = document.getElementById("btn-close-cross")
+const overlay = document.getElementById("overlay");
 
-openButton.addEventListener("click", function(){
+openButton.addEventListener("click", function(e){
+    console.log(e)
     modal.style.display = "block";
+    overlay.style.display = "block"
+    overlay.appendChild(modal)
 })
 
 closeButtonArrow.addEventListener("click", function(){
     modal.style.display = "none";
+    overlay.style.display = "none"
+    overlay.remove(modal)
 })
 
 closeButtonCross.addEventListener("click", function(){
     modal.style.display = "none";
+    overlay.style.display = "none"
+    overlay.remove(modal)
 })
 
 
